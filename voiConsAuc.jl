14e0acaafee69fd35d29019c2a8a34111832a4e0
wkmor1 <wkmor1@gmail.com>
2017-02-28 03:10:00 +0000

    bu

--- voiConsAuc.jl
+++ voiConsAuc.jl
@@ -1,4 +1,4 @@
-using Distributions, Optim, RCall, DataFrames
+using Distributions
 
 function evsi_an(μ = [0, 0], σ = [1, 1], nsamples = [10000, 10000])
     m   = μ[1] - μ[2]
@@ -47,38 +47,3 @@ function evsi_sim(μ = [0, 0], σ = [1, 1], nsamples = [10000, 10000]; nsims = 1
     mean(benefit) - maximum(μ)
 
 end
-
-function evsi_opt(b, μ, σ, f)
-    function fn(b, μ, σ, f)
-        g(p) = -f(μ, σ, [b * p, b * (1 - p)])
-        res  = optimize(g, 0, 1, method = GoldenSection())
-        return hcat(Optim.minimizer(res), -Optim.minimum(res))
-    end
-    res = hcat(b, vcat([fn(x, μ, σ, f) for x = b]...), vcat([f(μ, σ, [.5x, .5x]) for x = b]...))
-    res = convert(DataFrame, res)
-    rename!(res, Dict(:x1 => :n, :x2 => :p, :x3 => :evsi_opt, :x4 => :evsi_naive))
-end
-
-R"
-plot_evsi <- function(df, evpi) {
-        with(df,
-             {
-               par(mfrow = c(3, 1), las = 1, mar = c(0.5, 4, 0, 0), oma = c(5, 0, 0, 0))
-               plot(p ~ n, log = 'x', type = 'l', ylab = 'p', lwd = 2, ylim = 0:1, xaxt = 'n', xlab = '')
-               abline(h = .5, lwd = 2, lty = 2, col = 'grey')
-               plot(evsi_opt ~ n, log = 'x', type = 'l', lwd = 2, ylim = c(0, evpi), xaxt = 'n', xlab = '')
-               points(n, evsi_naive, type = 'l', lwd = 2, lty = 2, col = 'grey')
-               plot((evsi_opt - evsi_naive) ~ n, log = 'x', type = 'l', lwd = 2, ylim = c(0, evpi), xlab = '')
-               mtext('n', 1, 3, outer = TRUE)
-             }
-        )
-    }
-"
-
-function evsi_plot(μ = [0, 0], σ = [1, 1]; f = evsi_an,
-                   b = exp(collect(linspace(log(1), log(5000), 20))))
-    df   = evsi_opt(b, μ, σ, f)
-    evpi = evsi_an(μ, σ)
-    R"plot_evsi($df, $evpi)"
-end
-    
